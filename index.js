490b62f755840f2de28a54044929a8ece5d740a5
Mohammad <mohd.abed@saugo360.com>
2020-08-04 15:58:48 +0300

    update readme

--- index.js
+++ index.js
@@ -6,7 +6,30 @@ const md = require("markdown-it")({
 });
 
 const headerIntro = "# Hi there 👋";
-const markdownContent = md.render(headerIntro);
+const headerSubIntro =
+  "## I am Mohammad Abed. I am a Front-End Engineer and a Javascript Enthusaist.";
+const explanationAboutMe = "Here is a bit about me:";
+const facts = [
+  "I'm a huge React enthusaist",
+  "Javascript is my second language",
+  "I am always looking to collaborate on opensoure projects",
+  "I probably only completed 90% of 90% of my personal projects",
+  "Check out my [portfolio](https://mhmdabed.dev)"
+];
+
+const factsString = facts.reduce((acc, fact) => acc + `- ${fact}\n`, ``);
+
+const content = `
+
+${headerIntro}\n
+
+${headerSubIntro}\n
+
+${explanationAboutMe}\n
+
+${factsString}
+`;
+const markdownContent = md.render(content);
 
 fs.writeFile("README.md", markdownContent, err => {
   if (err) {
